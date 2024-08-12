b2aa230a62b138af7a99c7ec742c566410f24f6a
arg387 <84933329+arg387@users.noreply.github.com>
2024-08-12 23:11:03 +0530

    practice

--- new/user_weight.py
+++ new/user_weight.py
@@ -17,3 +17,32 @@ elif Planet == 7:
   print('destination weight = '+ str(Earth_Weight * 1.14))
 else:
   print('Invalid planet number')
+  #Alternative
+""" 
+earth_weight = float(input('What is your earth weight:'))
+Planet = int(input('What is the planet number?:'))
+
+if Planet == 1:
+  destination_weight = earth_weight * 0.38
+  print(destination_weight)
+elif Planet == 2:
+  destination_weight = earth_weight * 0.91
+  print(destination_weight)
+elif Planet == 3:
+  destination_weight = earth_weight * 0.38
+  print(destination_weight)
+elif Planet == 4:
+  destination_weight = earth_weight * 2.53
+  print(destination_weight)
+elif Planet == 5:
+ destination_weight = earth_weight * 1.07
+ print(destination_weight)
+elif Planet == 6:
+  destination_weight = earth_weight * 0.89
+  print(destination_weight)
+elif Planet == 7:
+  destination_weight = earth_weight * 1.14
+  print(destination_weight)
+else:
+  print('Invalid planet number')
+ """
