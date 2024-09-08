8e0b0c7c57865c32ee185770d12be1114f65a6ed
arg387 <84933329+arg387@users.noreply.github.com>
2024-09-08 20:49:56 +0530

    practice

--- new/functions/dog_years.py
+++ new/functions/dog_years.py
@@ -1,7 +1,7 @@
-""" def dog_years(name,age):
+def dog_years(name,age):
   new_age = 7 * age
   return F'{name} is {new_age} years old in human years.'
 print(dog_years('Landon', 3))
 print(dog_years('Red Bean', 6))
-print(dog_years('Cooper', 2)) """
+print(dog_years('Cooper', 2))
 
--- new/tempCodeRunnerFile.py
+++ new/tempCodeRunnerFile.py
@@ -1,1 +1,25 @@
-
+  import random 
+    items_list = [' 🍒 ',' 🍇 ',' 🍉 ',' 7️⃣ ']
+    print('--------------------')
+    name = input("What's your name? Dear Player ....")
+    play = input(F'Do you want to play? Dear {name} (yes/no):')
+    print('--------------------')
+    print('Loading...')
+    print('Loading...')
+    print('Loading...')
+    if play.lower() == 'yes':
+      for i in range(3):
+        new_list = []
+        for item in range(len(items_list) - 1):
+          new_list.append(random.choice(items_list))
+        print('--------------------')
+        print(F' {new_list[0]} | {new_list[1]} | {new_list[2]} ')
+        print('--------------------')
+        if new_list.count('7') == 3:
+          print("Jackpot! 💰")
+        else:
+          print("Thanks for playing!")
+    else:
+      print("Thanks for visiting!")
+    print('--------------------')
+    print('The End.')
