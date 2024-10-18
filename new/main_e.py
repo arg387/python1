9d230f9ad88209d9570313b3e5385d16d9042b0e
arg387 <84933329+arg387@users.noreply.github.com>
2024-10-18 10:19:44 +0530

    new addition

---
+++ new/main_e.py
@@ -0,0 +1,13 @@
+
+import datetime, bday_messages
+
+today = datetime.date.today()
+
+my_next_birthday = datetime.date(2024, 10, 17)
+
+days_away = my_next_birthday - today
+
+if my_next_birthday == today:
+  print(bday_messages.random_message)
+else:
+  print(f'My next birthday is {days_away.days} days away!')
